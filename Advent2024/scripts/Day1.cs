a616098bd54187d152d27305048af97bd650afd4
Rolu2907 <eric.rodriguez.luque@gmail.com>
2024-12-01 23:07:59 +0100

    Visual Studio and Menu

---
+++ Advent2024/scripts/Day1.cs
@@ -0,0 +1,15 @@
+namespace Advent_Of_Code_2023
+{
+    public class Day1
+    {
+        static public void P1(string[] input)
+        {
+            foreach (var line in input) {
+                Console.WriteLine(line);
+            }
+        }
+        static public void P2(string[] input)
+        {
+        }
+    }
+}
