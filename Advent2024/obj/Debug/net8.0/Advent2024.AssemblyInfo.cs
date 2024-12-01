7b489944143f4c3e8e201339dd237a8b3a36ebcd
Rolu2907 <eric.rodriguez.luque@gmail.com>
2024-12-01 22:22:47 +0100

    Menu

--- Advent2024/obj/Debug/net8.0/Advent2024.AssemblyInfo.cs
+++ Advent2024/obj/Debug/net8.0/Advent2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5752cc83d0929d25261f588640b29d2a28b4c96c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
