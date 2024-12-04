6d7b491e868f9ba317e3a7da24d9069ada6d5127
Rolu2907 <eric.rodriguez.luque@gmail.com>
2024-12-04 19:48:33 +0100

    Day 4 Part 2

--- Advent2024/scripts/Day4.cs
+++ Advent2024/scripts/Day4.cs
@@ -48,9 +48,34 @@ namespace Advent_Of_Code_2023
             }
             Console.WriteLine("Total: " + total);
         }
-        static void P2(string[] input)                   
+        static void P2(string[] input)
         {
+            int total = 0;
+            char[,] input2 = new char[input.Length, input[0].Length];
 
+            for (int i = 0; i < input2.GetLength(0); i++)
+            {
+                for (int j = 0; j < input2.GetLength(1); j++)
+                {
+                    input2[i, j] = '.';
+                }
+            }
+            for (int i = 1; i < input.Length - 1; i++)
+            {
+                for (int j = 1; j < input[i].Length - 1; j++)
+                {
+                    if (CheckX_MAS(input, i, j, ref input2)) total++;
+                }
+            }
+            for (int i = 0; i < input2.GetLength(0); i++)
+            {
+                for (int j = 0; j < input2.GetLength(1); j++)
+                {
+                    Console.Write(input2[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Total: " + total);
         }
         static bool CheckXMAS(string[] input, int line, int index, int direction, ref char[,] input2)
         {
@@ -202,6 +227,27 @@ namespace Advent_Of_Code_2023
                     }
                     break;
             }
+            return false;
+        }
+        static bool CheckX_MAS(string[] input, int line, int index, ref char[,] input2)
+        {
+            if (input[line][index] != 'A') return false;
+
+            char[] diag1 = [input[line - 1][index - 1], input[line + 1][index + 1]];
+            char[] diag2 = [input[line - 1][index + 1], input[line + 1][index - 1]];
+
+            input2[line, index] = input[line][index];
+
+            if (diag1.Contains('S') && diag1.Contains('M') && diag2.Contains('S') && diag2.Contains('M'))
+            {
+                input2[line - 1, index - 1] = input[line - 1][index - 1];
+                input2[line + 1, index + 1] = input[line + 1][index + 1];
+                input2[line - 1, index + 1] = input[line - 1][index + 1];
+                input2[line + 1, index - 1] = input[line + 1][index - 1];
+
+                return true;
+            }
+
             return false;
         }
     }
